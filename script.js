5ef2fbd8e5c475e0b7cb6f71df77a66841ad1ba0
Andra <andra.popa1989@gmail.com>
2018-12-27 22:13:41 +0000

    add how it works section and change background for navigation on scroll

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let nav = document.getElementById('nav');
+let navlink = document.querySelectorAll('nav-link');
 
 const scroll = () => {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        nav.style.backgroundColor = '#4776e6';
+        nav.style.backgroundImage = 'linear-gradient(90deg, #4776e6 0%, #8E54E9 100%)';
+        navlink.style.color = '#4776e6';
     } else {
-        nav.style.backgroundColor = 'transparent';
-        nav.style.color = '#ffffff';
+        nav.style.backgroundImage = 'linear-gradient(90deg, transparent 0%, transparent 100%)';
     }
 }
 
