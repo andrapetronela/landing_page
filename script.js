826cf546a9a3c3186a92533a0341d43d0222f076
Andra <andra.popa1989@gmail.com>
2018-12-27 20:40:55 +0000

    fix navigation on scroll

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let nav = document.getElementById('nav');
 const scroll = () => {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         nav.style.backgroundColor = '#4776e6';
-        nav.style.zIndex = '10000';
     } else {
         nav.style.backgroundColor = 'transparent';
         nav.style.color = '#ffffff';
