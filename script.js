b3d6b3db2e25d0776ebb05799e47c008d9f00872
Andra <andra.popa1989@gmail.com>
2019-01-06 22:58:02 +0000

    fix line between numbers - overview section

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const nav = document.getElementById('nav');
+const ctaBtn = document.getElementById('cta-nav-button');
 
 const scroll = () => {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         nav.style.backgroundImage = 'linear-gradient(90deg, #4776e6 0%, #8E54E9 100%)';
+        ctaBtn.style.border = '2px solid #ffffff';
     } else {
         nav.style.backgroundImage = 'linear-gradient(90deg, transparent 0%, transparent 100%)';
+        ctaBtn.style.border = '2px solid #8E54E9';
     }
 }
 
